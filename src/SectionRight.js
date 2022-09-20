7708e350611d4549625b6456228d843eebdcf642
Wasi <osiur2@gmail.com>
2022-09-20 07:30:42 +0600

    Add Section Page

--- src/SectionRight.js
+++ src/SectionRight.js
@@ -1,27 +1,18 @@
 import {sectionRmenu} from "./data"; 
 
 function SectionMenu ({menu}){
-    
     return (
-        <div className="w-full">
-            <span className= "p-1 m-2 text-xl font-bold hover:text-blue-800 ">{menu}</span>
-        </div>
+        <span className= "p-1 m-2 text-xl font-bold hover:text-blue-800 ">{menu}</span>   
     );
 }
 export const sMenu = sectionRmenu.map (e => <SectionMenu menu = {e}/>);
 
 function SectionRight ({num, heading}){
-    
     return (
-
-            <div className="w-[25%] h-[90%] p-5 mt-3 mr-5 divide-y-2 ">
-                               
-                <div className="w-full m-3">
-                    <p className="text-5xl font-bold text-gray-500 mt-4">{num}</p>
-                    <p className="text-2xl font-bold mt-4 tracking-tight">{heading}</p>
-                </div>
-                
-            </div>
+        <div className="w-full m-3">
+            <p className="text-5xl font-bold text-gray-500 mt-4">{num}</p>
+            <p className="text-2xl font-bold mt-4 tracking-tight">{heading}</p>
+        </div>      
     );
 }
 export default SectionRight;
